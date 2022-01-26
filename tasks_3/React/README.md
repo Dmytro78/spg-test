6527fbe30535ffaeee26ede55486ba6df6f1dacf
dmytro78 <fttravel78@gmail.com>
2022-01-26 07:27:59 +0200

    1

--- tasks_3/React/README.md
+++
@@ -1,9 +0,0 @@
-Getting Started
-
-"frontend launch"
-cd client
-npm start
-
-"backend launch"
-cd server
-npm start
--- tasks_3/Vue2/app/src/views/User.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="user">
-      <h1>information is absent ¯\_(ツ)_/¯</h1>   
-  </div>
-</template>
